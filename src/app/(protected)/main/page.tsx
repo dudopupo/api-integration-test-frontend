9e292f08f24e485c7382f08da1f6681259fdf1e7
lyntik <a-c-maksimenko@yandex.ru>
2025-03-02 21:08:44 +0300

    feat(sidebar): add base-api

--- src/app/(protected)/main/page.tsx
+++ src/app/(protected)/main/page.tsx
@@ -1,12 +1,8 @@
-'use client'
-import { useAuthStore } from "@/features/auth/store";
-
 export default function Main() {
-    const { logout } = useAuthStore()
+
     return (
       <div>
         <h1 className="text-2xl font-bold">Main Page</h1>
-        <button onClick={logout}>Logout</button>
       </div>
     );
   }
